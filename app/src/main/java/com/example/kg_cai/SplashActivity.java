62ec1d57c591965666ea9d8ad0f0734789cd3e4b
Erick Resurreccion <resurreccionericks@gmail.com>
2022-06-04 13:26:37 +0800

    Created splash activity

---
+++ app/src/main/java/com/example/kg_cai/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.kg_cai;
+
+import static java.lang.Thread.sleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView imgLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imgLogo = findViewById(R.id.imgLogo);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.logoanim);
+
+        imgLogo.setAnimation(anim);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    sleep(3000);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+    }
+}
