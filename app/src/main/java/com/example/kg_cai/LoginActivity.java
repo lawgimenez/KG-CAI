9f823015d0425ce98984184c39ea0479615a1dd7
Erick Resurreccion <resurreccionericks@gmail.com>
2022-06-28 14:41:27 +0800

    Successfully create a login feature connected in database

--- app/src/main/java/com/example/kg_cai/LoginActivity.java
+++ app/src/main/java/com/example/kg_cai/LoginActivity.java
@@ -1,15 +1,30 @@
 package com.example.kg_cai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText txtEmail, txtPassword, btnRegister;
     private Button btnLogin;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +33,57 @@ public class LoginActivity extends AppCompatActivity {
 
         btnLogin = findViewById(R.id.btnLogin);
 
+        txtEmail = findViewById(R.id.txtLoginEmail);
+        txtPassword = findViewById(R.id.txtLoginPass);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                String email = txtEmail.getText().toString();
+                String password = txtPassword.getText().toString();
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    txtEmail.setError("Please Enter A Valid Email Address");
+                    txtEmail.requestFocus();
+                    return;
+                }
+                if(email.isEmpty()||password.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Please enter all fields", Toast.LENGTH_SHORT).show();
+                }
+                if(password.length() < 6){
+                    txtPassword.setError("Please enter 6 digits password");
+                    txtPassword.requestFocus();
+                    return;
+                }
+
+                firebaseLogin(email,password);
+
+            }
+        });
+
+        if(firebaseAuth.getCurrentUser()!=null){ //if user is currently logged in it will go to main activity
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+    }
+
+    private void firebaseLogin(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
