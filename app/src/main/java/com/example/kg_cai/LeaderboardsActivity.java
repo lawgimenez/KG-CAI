e1a866735eda132a8e5437b5d60cd21fcaf03c55
Erick Resurreccion <resurreccionericks@gmail.com>
2022-08-20 14:21:53 +0800

    Add video background in some activities (as per our thesis adviser said, to get the attention of the kids)

--- app/src/main/java/com/example/kg_cai/LeaderboardsActivity.java
+++ app/src/main/java/com/example/kg_cai/LeaderboardsActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -95,9 +97,17 @@ public class LeaderboardsActivity extends AppCompatActivity {
 //        })
 
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) { //this is for back button
         if (item.getItemId() == android.R.id.home) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
             LeaderboardsActivity.this.finish();
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/kg_cai/LessonActivity.java
+++ app/src/main/java/com/example/kg_cai/LessonActivity.java
@@ -8,8 +8,15 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.VideoView;
+
 import com.example.kg_cai.helpers.LessonModel;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,7 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LessonActivity extends AppCompatActivity {
-
+    private VideoView videoMain;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference mReference;
     private RecyclerView lessonRV;
@@ -32,6 +39,7 @@ public class LessonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
+        playBackgroundVideo();
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         mReference = firebaseDatabase.getReference().child("Subjects").child(subject_picked).child("Lessons");
@@ -71,5 +79,54 @@ public class LessonActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void playBackgroundVideo() {
+        videoMain = findViewById(R.id.lessonVideoMain);
+        String path = "android.resource://"+getPackageName()+"/"+R.raw.circle_bg;
+        Uri uri = Uri.parse(path);
+        videoMain.setVideoURI(uri);
+        videoMain.start();
+
+        videoMain.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        videoMain.resume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        videoMain.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        videoMain.stopPlayback();
+        super.onDestroy();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(), LessonTitleActivity.class));
+        LessonActivity.this.finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) { //this is for back button
+        if (item.getItemId() == android.R.id.home) {
+            startActivity(new Intent(getApplicationContext(), LessonTitleActivity.class));
+            LessonActivity.this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
